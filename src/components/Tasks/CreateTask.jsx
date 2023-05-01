49d7054d643b88af658dff9095aeaf23268a02a5
lizzy596 <lizmeek123@gmail.com>
2023-05-01 11:33:44 -0500

    added create task message

--- src/components/Tasks/CreateTask.jsx
+++ src/components/Tasks/CreateTask.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { taskService } from '../../services/task.service';
 import { useUserAuth } from '../../services/auth.service';
+import TaskMessage from './TaskMessage';
 
 const CreateTask = () => {
   const userValue = useUserAuth();
+  const [showTaskMessage, setShowTaskMessage] = useState(false)
   const [dueDate, setDueDate] = useState(new Date());
-
   const [formData, setFormData] = useState({
     title: '',
   });
@@ -22,6 +23,8 @@ const CreateTask = () => {
       [name]: value,
     });
   };
+
+
 
   const handleDateChange = (date) => {
     setDueDate(date);
@@ -41,42 +44,61 @@ const CreateTask = () => {
     });
 
     setDueDate(new Date());
+    setShowTaskMessage(true);
   };
 
+
+  useEffect(() => {
+    let timeout;
+    timeout = setTimeout(() => {
+      setShowTaskMessage(false)
+
+    }, 2000)
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [showTaskMessage])
+
   return (
-    <form onSubmit={handleSubmit} className='max-w-md mx-auto'>
-      <div className='mb-4'>
-        <label htmlFor='title' className='block text-gray-700 font-bold mb-2'>
-          Title
-        </label>
-        <input
-          type='text'
-          id='title'
-          name='title'
-          value={formData.title}
-          onChange={handleInputChange}
-          className='shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline'
-        />
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='dueDate' className='block text-gray-700 font-bold mb-2'>
-          Due Date
-        </label>
-        <DatePicker
-          id='dueDate'
-          name='dueDate'
-          selected={dueDate}
-          onChange={handleDateChange}
-          className='shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline'
-        />
-      </div>
-      <button
-        type='submit'
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-      >
-        Create Task
-      </button>
-    </form>
+    <>
+      {showTaskMessage && <TaskMessage />}
+      <form onSubmit={handleSubmit} className='max-w-md mx-auto'>
+        <div className='mb-4'>
+          <label htmlFor='title' className='block text-gray-700 font-bold mb-2'>
+            Title
+          </label>
+          <input
+            type='text'
+            id='title'
+            name='title'
+            value={formData.title}
+            onChange={handleInputChange}
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline'
+          />
+        </div>
+        <div className='mb-4'>
+          <label
+            htmlFor='dueDate'
+            className='block text-gray-700 font-bold mb-2'
+          >
+            Due Date
+          </label>
+          <DatePicker
+            id='dueDate'
+            name='dueDate'
+            selected={dueDate}
+            onChange={handleDateChange}
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline'
+          />
+        </div>
+        <button
+          type='submit'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          Create Task
+        </button>
+      </form>
+    </>
   );
 };
 
---
+++ src/components/Tasks/TaskMessage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TaskMessage = () => {
+  return <h4 className='text-center text-violet-700 font-bold'>Task created!!</h4>;
+}
+
+export default TaskMessage;
